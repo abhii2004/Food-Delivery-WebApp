12352e326f015a782a9eee39423e6380cea45222
Abhijit Patil <117617903+abhii2004@users.noreply.github.com>
2025-09-03 22:40:57 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://abhijitpatil2004:abhijitpatil2004@cluster0.cdvpy.mongodb.net/food-del').then(() => console.log("DB Connected"));
+    await mongoose.connect('your URL from mongoose').then(() => console.log("DB Connected"));
 }
